aac30b1c469b7cf481e471f9c28558490da8f13a
Beatrix <48468964+esraertsak@users.noreply.github.com>
2021-05-08 13:34:36 +0300

    Update 3sayilartoplami.cpp

--- Esra/3sayilartoplami.cpp
+++ Esra/3sayilartoplami.cpp
@@ -3,9 +3,14 @@ using namespace std;
 int main(){
 
     int n,total;
+    total=0;
     cout<<"Please write the number:"<<endl;
     cin>>n;
-    total=(n*(n+1))/2;
+   while(n>0){
+       n=n+1;
+       total=total+n;
+
+   }
     cout<<"Total of the sequential number:"<<total<<endl;
-    
+
 }
