b9e0fee4f029737643399e4409b4eeba48befa5e
Mustafa Emre <m-emrekalaycik@hotmail.com>
2021-05-12 16:49:24 +0300

    dosyalar numaralandırıldı

---
+++ Mustafa Emre/02) toplama.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Ilk sayiyi giriniz:");
+    scanf("%d",&a);
+    printf(" \nIkinci sayiyi giriniz:");
+    scanf("%d",&b);
+    printf("\nGirdigin sayilarin toplami: %d",a+b);
+    return 0;
+}
