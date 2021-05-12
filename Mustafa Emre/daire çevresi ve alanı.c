0f4a4fd5be2320eec921d6dcbee9bfafaa84c78c
Mustafa Emre <m-emrekalaycik@hotmail.com>
2021-05-12 13:16:50 +0300

    dairenin alanını ve çevresini hesaplayan program hazır

---
+++ Mustafa Emre/daire çevresi ve alanı.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define pi 3.14159265358979323846
+
+int main(){
+    float r;
+	printf("dairenin yaricapi: ");
+	scanf("%f",&r);
+    printf("\ndairenin cevresinin uzunlugu:%f",(2*pi*r));
+	printf("\ndairenin cevresinin uzunlugu:%f",(pi*r*r));
+    return 0;
+}
