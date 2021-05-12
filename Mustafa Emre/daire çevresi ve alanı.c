44233088c52589e8629673f9687090d0d5ae91f8
Mustafa Emre <m-emrekalaycik@hotmail.com>
2021-05-12 13:20:07 +0300

    eklemeyi unuttuğum ufak bişey ekledim

--- Mustafa Emre/daire çevresi ve alanı.c
+++ Mustafa Emre/daire çevresi ve alanı.c
@@ -5,7 +5,12 @@ int main(){
     float r;
 	printf("dairenin yaricapi: ");
 	scanf("%f",&r);
-    printf("\ndairenin cevresinin uzunlugu:%f",(2*pi*r));
-	printf("\ndairenin cevresinin uzunlugu:%f",(pi*r*r));
+    if (r<0){
+        printf("negatif uzunluk olmaz.")
+    }
+    else{
+        printf("\ndairenin cevresinin uzunlugu:%f",(2*pi*r));
+	    printf("\ndairenin cevresinin uzunlugu:%f",(pi*r*r));
+    }
     return 0;
 }
