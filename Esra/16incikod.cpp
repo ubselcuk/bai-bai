f11d4291aaf852e5294527ea26091f9b8a94511b
Beatrix <48468964+esraertsak@users.noreply.github.com>
2021-05-20 16:18:51 +0300

    16.kod

--- Esra/16incikod.cpp
+++ Esra/16incikod.cpp
@@ -12,8 +12,8 @@ int main()
       std::cin>>i;
       toplam=toplam+i;
       kacsayi=0;
-      kacsayi++;
  }
+      kacsayi++;
  ortalama=toplam/kacsayi;
  std::cout<<"ortalama:"<<ortalama<<std::endl;
     
