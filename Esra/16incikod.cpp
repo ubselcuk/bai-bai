972507d26b86486c843a7449b094dbf5fc37eb49
Beatrix <48468964+esraertsak@users.noreply.github.com>
2021-05-20 16:04:54 +0300

    16.kod

---
+++ Esra/16incikod.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main()
+{   
+ 
+ int i,kacsayi;
+ float ortalama;
+ int toplam=0;
+ 
+ while(i != -1){
+      std::cout<<"Sayi girin:"<<std::endl;
+      std::cin>>i;
+      toplam=toplam+i;
+      kacsayi=0;
+      kacsayi++;
+ }
+ ortalama=toplam/kacsayi;
+ std::cout<<"ortalama:"<<ortalama<<std::endl;
+    
+}
