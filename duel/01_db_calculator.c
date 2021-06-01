39f75ffd64f3d2f2dbd795dbdeae2c019f91bbd5
Denis Kaynar <kaynar.denis@gmail.com>
2021-06-01 17:21:10 +0300

    4 islem yapildi

--- duel/01_db_calculator.c
+++ duel/01_db_calculator.c
@@ -2,28 +2,67 @@
 
 #include <stdio.h>
 
+double add(double a, double b){
+  return a+b;
+}
+
+double sub(double a, double b){
+  return a-b;
+}
+
+double div(double a, double b){
+  return a/b;
+}
+
+double mul(double a, double b){
+  return a*b;
+}
+
 int main()
 {
     char opr;
 
     double x;
     double y;
+    double result;
 
     printf("What do you want to do? (+ - / *) > ");
     scanf("%c", &opr);
 
+    printf("First number: ");
+    scanf("%lf", &x);
+
+    printf("Second number: ");
+    scanf("%lf", &y);
+
     switch (opr)
     {
     case '+':
+
+        result = add(x,y);
+        printf("%lf + %lf = %lf", x, y, result);
+
         break;
 
     case '-':
+
+        result = sub(x,y);
+        printf("%lf - %lf = %lf", x, y, result);
+
         break;
 
     case '/':
+
+        result = div(x,y);
+        printf("%lf / %lf = %lf", x, y, result);
+
         break;
 
     case '*':
+
+        result = mul(x,y);
+        printf("%lf * %lf = %lf", x, y, result);
+
         break;
 
     default:
