6d5d409d3908f865a6cc78ae6c5e51bfece4ba92
ubselcuk <ubselcuk@gmail.com>
2021-06-01 14:49:35 +0300

    menü yapıldı

---
+++ duel/01_db_calculator.c
@@ -0,0 +1,34 @@
+// Denis & Boris simple calculator
+
+#include <stdio.h>
+
+int main()
+{
+    char opr;
+
+    double x;
+    double y;
+
+    printf("What do you want to do? (+ - / *) > ");
+    scanf("%c", &opr);
+
+    switch (opr)
+    {
+    case '+':
+        break;
+
+    case '-':
+        break;
+
+    case '/':
+        break;
+
+    case '*':
+        break;
+
+    default:
+        printf("task failed successfully");
+    }
+
+    return 0;
+}
