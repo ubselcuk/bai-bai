92f53e67b9ef94fdc0806cfdea637148a7fc25e4
mactorient <mertemir38@hotmail.com>
2021-07-06 16:19:42 +0300

    kod tamamen değişti

--- merte/python/23.ascii.py
+++ merte/python/23.ascii.py
@@ -1,8 +1,18 @@
-import curses.ascii as ascii
-metin = list(input('Metin girin: '))
+import string
 
-for i in metin:
-    if ascii.islower(i):
-        print(f'{i} : {ord(i)}')
-    else:
-        print(f'{i} : {ord(i)}')
+lowercase= list(string.ascii_lowercase+"üğşçöı")
+ascii_table= {}
+
+for i in range(len(lowercase)):
+    ascii_table.update({lowercase[i] : ord(lowercase[i])})
+
+user_input= list(input('Metin girin: '))
+
+metin=''
+for letter in user_input:
+    if letter in lowercase:
+        letter= letter.upper()
+    metin+=letter
+
+print(metin)
+
