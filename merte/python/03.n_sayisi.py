59dd5a2079ffa780313c13a85addcf646e15c5c6
mactorient <mertemir38@hotmail.com>
2021-07-06 15:14:47 +0300

    range fonksiyonuna +1 eklendi

--- merte/python/03.n_sayisi.py
+++ merte/python/03.n_sayisi.py
@@ -1,6 +1,9 @@
 n = int(input("Bir doğal sayı girin: "))
 
 toplam=0
-for i in range(1, n):
-	toplam += i
-print(toplam)
+if n>0:
+	for i in range(1, n+1):
+		toplam+=i
+	print(f"{n}'e kadar olan sayilarin toplami = {toplam}")
+else:
+	print('Lutfen bir doğal sayi girin')
