83620523b660eb7b590b451d5fd27771d46c9a38
mactorient <mertemir38@hotmail.com>
2021-09-11 16:43:59 +0300

    2 sayiyi toplama toplama

---
+++ merte/cpp/2.toplama.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x,y;
+    
+    cout << "1. Sayiyi girin: ";
+    cin >> x;
+
+    cout << "2. Sayiyi girin: ";
+    cin >> y;
+
+    cout << "Toplam = " << x+y;
+}
